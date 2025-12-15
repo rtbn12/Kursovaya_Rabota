4b3616f46eaa55d02ec74dda2fe60f2a985e9105
Egor Solodov <solodov_2007@bk.ru>
2025-12-15 12:33:53 +0300

    Разработал класс Peripheral

---
+++ src/Peripheral.java
@@ -0,0 +1,15 @@
+public class Peripheral extends Product{
+
+
+
+    public Peripheral(String id, String brand, String model,
+                      String countryProduction, int productionDate,
+                      int price, String category, String description,
+                      int warrantyMoths) {
+
+
+        super(id, brand, model, countryProduction,
+                productionDate, price, category,
+                description, warrantyMoths);
+    }
+}
